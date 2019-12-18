0338f339d3229812933f0b281060d145bac1ef30
Edward Yun <nedinsuyun@gmail.com>
2019-12-18 17:55:53 -0500

    navigation for components works

--- src/app/app-components/header/header.component.ts
+++ src/app/app-components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  router: Router;
   signedIn: boolean = false;
   constructor() { }
 
