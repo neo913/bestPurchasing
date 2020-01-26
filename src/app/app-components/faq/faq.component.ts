5e982e6248aaf4d485144e52ca03546fb0a3f09f
Edward Yun <nedinsuyun@gmail.com>
2020-01-25 20:05:23 -0400

    fixed faq

--- src/app/app-components/faq/faq.component.ts
+++ src/app/app-components/faq/faq.component.ts
@@ -5,7 +5,6 @@ interface faqItem {
   category: string,
   answer: string
 }
-
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
@@ -13,22 +12,24 @@ interface faqItem {
 })
 export class FaqComponent implements OnInit {
 
-  constructor() { }
-
   faqItem: faqItem;
   faqList: faqItem[];
+  category = '';
+
+  constructor() { }
 
   ngOnInit() {
     this.faqList = new Array<faqItem>();
     this.testGenerator();
+    this.category = '';
   }
 
   testGenerator() {
-    this.addItem('Test Question', 'Category1', "Test Answer");
-    this.addItem('Test Question', 'Category1', 'Test Answer');
-    this.addItem('Test Question', 'Category1', 'Test Answer');
-    this.addItem('Test Question', 'Category1', 'Test Answer');
-    this.addItem('Test Question', 'Category1', 'Test Answer');
+    this.addItem('How I can access to my account?', 'Account', "Sorry. The menu is not available yet.");
+    this.addItem('Where is my package?', 'Shop', 'Sorry. We are woring on it.');
+    this.addItem('What is your phone number?', 'Contact', 'Please check Contact page.');
+    this.addItem('I cannot buy the product', 'Shop', 'Sorry. The page is in progress.');
+    this.addItem('Your logo looks silly.', 'Web', 'Sorry. I did my best. :)');
   }
 
   addItem(question:string, category: string, answer: string) {
@@ -41,6 +42,12 @@ export class FaqComponent implements OnInit {
     
     this.faqList.push(faqItem);
   }
-  
 
+  categoryFilter(category: string) {
+    if(!this.category) { return true;
+    } else {
+      if(this.category == category) { return true; }
+      else {return false;}
+    }
+  }
 }
